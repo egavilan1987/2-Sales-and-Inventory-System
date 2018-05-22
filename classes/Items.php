85c91a0f805c71194f17a98177b76e07a261c321
egavilan1987 <enmanuelgc@gmail.com>
2018-05-22 07:34:19 -0400

    Video 20

---
+++ classes/Items.php
@@ -0,0 +1,47 @@
+
+
+<?php 
+	class items{
+		public function addImage($data){
+			$c=new Connect();
+			$connection=$c->connection();
+
+			$date=date('Y-m-d');
+
+			$sql="INSERT INTO sl_images (id_categorY,
+										name_image,
+										path,
+										uploaded_date)
+							VALUES ('$data[0]',
+									'$data[1]',
+									'$data[2]',
+									'$date')";
+
+
+			return mysqli_query($connection,$sql);
+		}
+			public function insertItem($data){
+			$c=new Connect();
+			$connection=$c->connection();
+
+			$date=date('Y-m-d');
+
+			$sql="INSERT INTO sl_items (id_category,
+										id_image,
+										id_user,
+										name_product,
+										description_product,
+										stock_product,
+										price_product,
+										date_capture) 
+							VALUES ('$data[0]',
+									'$data[1]',
+									'$data[2]',
+									'$data[3]',
+									'$data[4]',
+									'$data[5]',
+									'$data[6]',
+									'$date')";
+			return mysqli_query($connection,$sql);
+		}
+	}
--- process/items/insertItem.php
+++ process/items/insertItem.php
@@ -1,6 +1,12 @@
-<?php
 
-	$imageName=$_FILES['image']['name'];
+
+<?php
+	session_start();
+	$idUser=$_SESSION['user'];
+	require_once "../../classes/connection.php";
+	require_once "../../classes/Items.php";
+
+	$obj= new items();
 
 	$data=array(
 		$_POST['selectCategory'],
@@ -8,9 +14,39 @@
 		$_POST['description'],
 		$_POST['quantity'],
 		$_POST['price'],
-		$imageName
 
 	);
-	print_r($data);
+
+	$imageName=$_FILES['image']['name'];
+	$storagePath=$_FILES['image']['tmp_name'];
+	$folder='../../files/';
+	$finalPath=$folder.$imageName;
+
+	$dataImg=array(
+		$_POST['selectCategory'],
+		$imageName,
+		$finalPath
+					);
+
+
+		if(move_uploaded_file($storagePath, $finalPath)){
+			$idImage=$obj->addImage($dataImg);
+
+				if($idImage > 0){
+
+					$data[0]=$_POST['selectCategory'];
+					$data[1]=$idImage;
+					$data[2]=$idUser;
+					$data[3]=$_POST['name'];
+					$data[4]=$_POST['description'];
+					$data[5]=$_POST['quantity'];
+					$data[6]=$_POST['price'];
+					echo $obj->insertItem($data);
+				}else{
+					echo 0;
+				}			
+
+		}
+
 
 ?>
