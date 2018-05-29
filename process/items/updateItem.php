8e8ac583605ceb8fea1323cdf3e57f05cc8ce5da
egavilan1987 <enmanuelgc@gmail.com>
2018-05-29 11:27:14 -0400

    Update updateItem.php

--- process/items/updateItem.php
+++ process/items/updateItem.php
@@ -1,1 +1,17 @@
+<?php 
 
+	require_once "../../classes/connection.php";
+	require_once "../../classes/Items.php";
+
+	$obj= new items();
+
+$arrayItem=array(
+		$_POST['idArticulo'],
+	    $_POST['categoriaSelectU'],
+	    $_POST['nombreU'],
+	    $_POST['descripcionU'],
+	    $_POST['cantidadU'],
+	    $_POST['precioU']
+			);
+    echo $obj->updateItem($arrayItem);
+ ?>
