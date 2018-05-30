41b5692dd55a79a96ab6251ab835b2c5fae80c0f
egavilan1987 <enmanuelgc@gmail.com>
2018-05-30 11:51:20 -0400

    Update usersTable.php

--- view/users/usersTable.php
+++ view/users/usersTable.php
@@ -26,7 +26,7 @@
 		<td><?php echo $row[2]; ?></td>
 		<td><?php echo $row[3]; ?></td>
 		<td>
-			<span data-toggle="modal" data-target="#updateCategoryModal" class="btn btn-warning btn-xs" onclick="updateUser('<?php echo $row[0]; ?>')">
+			<span data-toggle="modal" data-target="#updateUserModal" class="btn btn-warning btn-xs" onclick="updateUser('<?php echo $row[0]; ?>')">
 				<span class="glyphicon glyphicon-pencil"></span>
 			</span>
 		</td>
