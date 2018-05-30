2c7f797bbfe770508621b84217847e9632be9262
egavilan1987 <enmanuelgc@gmail.com>
2018-05-30 01:07:42 -0400

    Revert "Update Items.php"
    This reverts commit 2ad5a0861b8b58e90a0dfaecb5c7404013daeeff.

--- classes/Items.php
+++ classes/Items.php
@@ -82,41 +82,44 @@ public function getItemData($idItem){
 						WHERE id_product='$data[0]'";
 			return mysqli_query($connection,$sql);
 		}
-public function deleteItem($idProduct){
+		public function deleteItem($idItem){
+			
 			$c=new Connect();
-			$conn=$c->connection();
-			$idImage=self::getImageID($idProduct);
-			$sql="DELETE from sl_items 
-					WHERE id_product='$idProduct'";
-			$result=mysqli_query($conn,$sql);
+			$connection=$c->connection();
+			
+			$idimagen=self::obtenIdImg($idItem);
+			
+			$sql="DELETE FROM sl_items 
+					WHERE id_product='$idItem'";
+			$result=mysqli_query($connection,$sql);
 			if($result){
-				$path=self::getImagePath($idImage);
-				$sql="DELETE from sl_images 
-						WHERE id_image='$idImage'";
-				$result=mysqli_query($conn,$sql);
+				$ruta=self::obtenRutaImagen($idimagen);
+				$sql="DELETE from imagenes 
+						where id_imagen='$idimagen'";
+				$result=mysqli_query($conexion,$sql);
 					if($result){
-						if(unlink($path)){
+						if(unlink($ruta)){
 							return 1;
 						}
 					}
 			}
 		}
-		public function getImageID($idProduct){
-			$c=new Connect();
-			$conn=$c->connection();
-			$sql="SELECT id_image 
-					FROM sl_items 
-					WHERE id_product='$idProduct'";
-			$result=mysqli_query($conn,$sql);
+		public function obtenIdImg($idProducto){
+			$c= new conectar();
+			$conexion=$c->conexion();
+			$sql="SELECT id_imagen 
+					from articulos 
+					where id_producto='$idProducto'";
+			$result=mysqli_query($conexion,$sql);
 			return mysqli_fetch_row($result)[0];
 		}
-		public function getImagePath($idImg){
-			$c=new Connect();
-			$conn=$c->connection();
-			$sql="SELECT path 
-					FROM sl_images 
-					WHERE id_image='$idImg'";
-			$result=mysqli_query($conn,$sql);
+		public function obtenRutaImagen($idImg){
+			$c= new conectar();
+			$conexion=$c->conexion();
+			$sql="SELECT ruta 
+					from imagenes 
+					where id_imagen='$idImg'";
+			$result=mysqli_query($conexion,$sql);
 			return mysqli_fetch_row($result)[0];
 		}
 	}
