1bdeadf2704fd4c404defcb74d5995e698a1388d
egavilan1987 <enmanuelgc@gmail.com>
2018-05-30 13:16:34 -0400

    Update updateUser.php

--- process/users/updateUser.php
+++ process/users/updateUser.php
@@ -1,12 +1,14 @@
 <?php 
-	require_once "../../clases/Conexion.php";
-	require_once "../../clases/Usuarios.php";
-	$obj= new usuarios;
-	$datos=array(
-			$_POST['idUsuario'],  
-		    $_POST['nombreU'] , 
-		    $_POST['apellidoU'],  
-		    $_POST['usuarioU']
+	session_start();
+	require_once "../../classes/connection.php";
+	require_once "../../classes/Users.php";
+
+	$obj= new users;
+	$userArray=array(
+			    $_POST['idUser'],  
+			    $_POST['nameUpdate'] , 
+			    $_POST['lastNameUpdate'],  
+			    $_POST['userUpdate']
 				);  
-	echo $obj->actualizaUsuario($datos);
+	echo $obj->updateUser($userArray);
  ?>
