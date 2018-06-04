5877bdc4e530c93417d9d4e72e8f2b577277cb40
egavilan1987 <enmanuelgc@gmail.com>
2018-06-04 14:20:54 -0400

    Update productSale.php

--- view/sales/productSale.php
+++ view/sales/productSale.php
@@ -1,40 +1,136 @@
-<h4>Sale an item</h4>
+<?php 
+require_once "../../clases/Conexion.php";
+$c= new conectar();
+$conexion=$c->conexion();
+?>
+
+
+<h4>Vender un producto</h4>
 <div class="row">
 	<div class="col-sm-4">
-		<form id="frmProductSale">
-			<label>Customers</label>
-			<select class="form-control input-sm" id="saleClient" name="saleClient">
-				<option value="A">Select</option>
-			</select>		
-			<label>Products</label>
-			<select class="form-control input-sm" id="saleProduct" name="saleProduct">
-				<option value="A">Select</option>
+		<form id="frmVentasProductos">
+			<label>Seleciona Cliente</label>
+			<select class="form-control input-sm" id="clienteVenta" name="clienteVenta">
+				<option value="A">Selecciona</option>
+				<option value="0">Sin cliente</option>
+				<?php
+				$sql="SELECT id_cliente,nombre,apellido 
+				from clientes";
+				$result=mysqli_query($conexion,$sql);
+				while ($cliente=mysqli_fetch_row($result)):
+					?>
+					<option value="<?php echo $cliente[0] ?>"><?php echo $cliente[2]." ".$cliente[1] ?></option>
+				<?php endwhile; ?>
 			</select>
-			<label>Descripction</label>
-			<textarea readonly="" id="descripctionSale" name="descripctionSale" class="form-control input-sm"></textarea>
-			<label>Quantity</label>
-			<input readonly="" type="text" class="form-control input-sm" id="quantitySale" name="quantitySale">
-			<label>Price</label>
-			<input readonly="" type="text" class="form-control input-sm" id="priceSale" name="priceSale">
+			<label>Producto</label>
+			<select class="form-control input-sm" id="productoVenta" name="productoVenta">
+				<option value="A">Selecciona</option>
+				<?php
+				$sql="SELECT id_producto,
+				nombre
+				from articulos";
+				$result=mysqli_query($conexion,$sql);
+				while ($producto=mysqli_fetch_row($result)):
+					?>
+					<option value="<?php echo $producto[0] ?>"><?php echo $producto[1] ?></option>
+				<?php endwhile; ?>
+			</select>
+			<label>Descripcion</label>
+			<textarea readonly="" id="descripcionV" name="descripcionV" class="form-control input-sm"></textarea>
+			<label>Cantidad</label>
+			<input readonly="" type="text" class="form-control input-sm" id="cantidadV" name="cantidadV">
+			<label>Precio</label>
+			<input readonly="" type="text" class="form-control input-sm" id="precioV" name="precioV">
 			<p></p>
-			<span class="btn btn-primary" id="btnAddSale">Add</span>
-			<span class="btn btn-danger" id="btnCancelSale">Cancel Sale</span>
+			<span class="btn btn-primary" id="btnAgregaVenta">Agregar</span>
+			<span class="btn btn-danger" id="btnVaciarVentas">Vaciar ventas</span>
 		</form>
 	</div>
 	<div class="col-sm-3">
-		<div id="imgProduct"></div>
+		<div id="imgProducto"></div>
 	</div>
 	<div class="col-sm-4">
-		<div id="loadTempSalesTable"></div>
+		<div id="tablaVentasTempLoad"></div>
 	</div>
 </div>
 
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('#tablaVentasTempLoad').load("ventas/tablaVentasTemp.php");
+		$('#productoVenta').change(function(){
+			$.ajax({
+				type:"POST",
+				data:"idproducto=" + $('#productoVenta').val(),
+				url:"../procesos/ventas/llenarFormProducto.php",
+				success:function(r){
+					dato=jQuery.parseJSON(r);
+					$('#descripcionV').val(dato['descripcion']);
+					$('#cantidadV').val(dato['cantidad']);
+					$('#precioV').val(dato['precio']);
+					$('#imgProducto').prepend('<img class="img-thumbnail" id="imgp" src="' + dato['ruta'] + '" />');
+				}
+			});
+		});
+		$('#btnAgregaVenta').click(function(){
+			vacios=validarFormVacio('frmVentasProductos');
+			if(vacios > 0){
+				alertify.alert("Debes llenar todos los campos!!");
+				return false;
+			}
+			datos=$('#frmVentasProductos').serialize();
+			$.ajax({
+				type:"POST",
+				data:datos,
+				url:"../procesos/ventas/agregaProductoTemp.php",
+				success:function(r){
+					$('#tablaVentasTempLoad').load("ventas/tablaVentasTemp.php");
+				}
+			});
+		});
+		$('#btnVaciarVentas').click(function(){
+		$.ajax({
+			url:"../procesos/ventas/vaciarTemp.php",
+			success:function(r){
+				$('#tablaVentasTempLoad').load("ventas/tablaVentasTemp.php");
+			}
+		});
+	});
+	});
+</script>
 
+<script type="text/javascript">
+	function quitarP(index){
+		$.ajax({
+			type:"POST",
+			data:"ind=" + index,
+			url:"../procesos/ventas/quitarproducto.php",
+			success:function(r){
+				$('#tablaVentasTempLoad').load("ventas/tablaVentasTemp.php");
+				alertify.success("Se quito el producto :D");
+			}
+		});
+	}
+	function crearVenta(){
+		$.ajax({
+			url:"../procesos/ventas/crearVenta.php",
+			success:function(r){
+				if(r > 0){
+					$('#tablaVentasTempLoad').load("ventas/tablaVentasTemp.php");
+					$('#frmVentasProductos')[0].reset();
+					alertify.alert("Venta creada con exito, consulte la informacion de esta en ventas hechas :D");
+				}else if(r==0){
+					alertify.alert("No hay lista de venta!!");
+				}else{
+					alertify.error("No se pudo crear la venta");
+				}
+			}
+		});
+	}
+</script>
 
 <script type="text/javascript">
 	$(document).ready(function(){
-		$('#saleClient').select2();
-		$('#saleProduct').select2();
-
+		$('#clienteVenta').select2();
+		$('#productoVenta').select2();
 	});
 </script>
